59b0dc218bc4f44b1815ddb6234855471542c6b8
Shikhar Agrawal <200070076@iitb.ac.in>
2021-10-22 12:13:45 +0530

    first commit on local repo

--- bobby/Moderator.java
+++ bobby/Moderator.java
@@ -16,6 +16,8 @@ public class Moderator implements Runnable{
 	public void run(){
 		while (true){
 			try{
+				board.moderatorEnabler.acquire();
+				board.threadInfoProtector.acquire();
 				/*acquire permits: 
 				
 				1) the moderator itself needs a permit to run, see Board
@@ -44,17 +46,17 @@ public class Moderator implements Runnable{
 				//base case
 				
 				if (this.board.embryo){
-					                              
-                                        
-                                   
-                                              
-					continue;
-				}
+					if(this.board.totalThreads>0) this.board.embryo=false;                    
+					else{
+						board.moderatorEnabler.release();
+						board.threadInfoProtector.release();
+						continue;
+					}
+				} //There was just a continue statement, we have added rest everything!
 				
 				
 				//find out how many newbies
-				int newbies = ;
-
+				int newbies = this.board.totalThreads - this.board.playingThreads + this.board.quitThreads;
 
 				/*
 				If there are no threads at all, it means Game Over, and there are no 
@@ -64,8 +66,15 @@ public class Moderator implements Runnable{
 				Thus, the moderator's job will be done, and this thread can terminate.
 				As good practice, we will release the "lock" we held. 
 				*/
+				if(board.totalThreads==0){
+					board.dead=true;
+					board.moderatorEnabler.release();
+					board.threadInfoProtector.release();
+				}
 
-				                                  
+				this.board.registration = new Semaphore(newbies);
+				this.board.reentry = new Semaphore(this.board.playingThreads - this.board.quitThreads);
+
                                               
             
      
@@ -80,8 +89,12 @@ public class Moderator implements Runnable{
 
 				Release permits for threads to play, and the permit to modify thread info
 				*/
-
-				                                                    
+				this.board.playingThreads=this.board.totalThreads;
+				this.board.quitThreads=0;
+				
+				board.threadInfoProtector.release();
+				
+				//Releasing the permits for the threads to play part is not done yet!                                       
                                
     
                                              
